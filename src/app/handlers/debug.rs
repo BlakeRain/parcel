c6a097355ce01967ac4def5e387b89d16545c832
Blake Rain <blake.rain@blakerain.com>
2024-11-07 10:37:43 +0000

    fix: remove dodgy transaction code

--- src/app/handlers/debug.rs
+++ src/app/handlers/debug.rs
@@ -4,18 +4,23 @@ use poem::{
     web::{Data, Json},
 };
 use serde::Deserialize;
+use sqlx::SqlitePool;
 use time::OffsetDateTime;
 
-use crate::{env::Env, model::user::hash_password};
+use crate::{
+    env::Env,
+    model::{
+        types::Key,
+        user::{hash_password, User},
+    },
+};
 
-async fn empty_tables(
-    mut tx: sqlx::Transaction<'_, sqlx::Sqlite>,
-) -> poem::Result<sqlx::Transaction<'_, sqlx::Sqlite>> {
+async fn empty_tables(pool: &SqlitePool) -> poem::Result<()> {
     const TABLE_NAMES: &[&str] = &["uploads", "team_members", "teams", "users"];
 
     for table_name in TABLE_NAMES.iter() {
         sqlx::query(&format!("DELETE FROM {}", table_name))
-            .execute(&mut *tx)
+            .execute(pool)
             .await
             .map_err(|err| {
                 tracing::error!(
@@ -27,27 +32,12 @@ async fn empty_tables(
             })?;
     }
 
-    Ok(tx)
+    Ok(())
 }
 
 #[poem::handler]
 async fn reset_database(env: Data<&Env>) -> poem::Result<()> {
-    let tx = env.pool.begin().await.map_err(|err| {
-        tracing::error!(
-            "Failed to begin database transaction in debug handler: {}",
-            err
-        );
-        InternalServerError(err)
-    })?;
-
-    if let Err(err) = empty_tables(tx).await?.commit().await {
-        tracing::error!(
-            "Failed to commit database transaction in debug handler: {}",
-            err
-        );
-        return Err(poem::Error::from_status(StatusCode::INTERNAL_SERVER_ERROR));
-    };
-
+    empty_tables(&env.pool).await?;
     Ok(())
 }
 
@@ -56,30 +46,30 @@ struct InitialUser {
     username: String,
     name: String,
     password: String,
+    #[serde(default, rename = "passwordHash")]
+    password_hash: Option<String>,
     admin: bool,
 }
 
 #[poem::handler]
 async fn initial_users(env: Data<&Env>, Json(users): Json<Vec<InitialUser>>) -> poem::Result<()> {
-    let tx = env.pool.begin().await.map_err(|err| {
-        tracing::error!(
-            "Failed to begin database transaction in debug handler: {}",
-            err
-        );
-        InternalServerError(err)
-    })?;
-
-    let mut tx = empty_tables(tx).await?;
+    empty_tables(&env.pool).await?;
 
     for user in users {
-        let hash = hash_password(&user.password);
-        sqlx::query("INSERT INTO users (username, name, password, enabled, admin, created_at) VALUES (?, ?, ?, 1, ?, ?)")
+        let hash = if let Some(hash) = user.password_hash {
+            hash
+        } else {
+            hash_password(&user.password)
+        };
+
+        sqlx::query("INSERT INTO users (id, username, name, password, enabled, admin, created_at) VALUES (?, ?, ?, ?, 1, ?, ?)")
+            .bind(Key::<User>::new())
             .bind(&user.username)
             .bind(&user.name)
             .bind(hash)
             .bind(user.admin)
             .bind(OffsetDateTime::now_utc())
-            .execute(&mut *tx)
+            .execute(&env.pool)
             .await.map_err(|err| {
                 tracing::error!(
                     "Failed to insert user {} in database in debug handler: {}",
