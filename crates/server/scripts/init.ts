726533fd1615d114dad19e8116e5215212202695
Blake Rain <blake.rain@blakerain.com>
2025-07-22 00:34:04 +0100

    feat: add upload tags

--- crates/server/scripts/init.ts
+++ crates/server/scripts/init.ts
@@ -2,10 +2,12 @@ import { register as register_baseurl } from "./components/baseurl";
 import { register as register_checklist } from "./components/checklist";
 import { register as register_clipboard } from "./components/clipboard";
 import { register as register_date } from "./components/date";
+import { register as register_dropdown } from "./components/dropdown";
 import { register as register_modal } from "./components/modal";
 import { register as register_select } from "./components/select";
 import { register as register_teams } from "./components/teams";
 import { register as register_dropdown } from "./components/dropdown";
+import { register as register_tag_input } from "./components/tag-input";
 
 function init() {
   if (window.customElements) {
@@ -19,10 +21,12 @@ function init() {
   register_checklist();
   register_clipboard();
   register_date();
+  register_dropdown();
   register_modal();
   register_select();
   register_teams();
   register_dropdown();
+  register_tag_input();
 
   document.addEventListener("click", (event) => {
     if ((event.target as HTMLElement).closest(".dismiss-skip")) {
