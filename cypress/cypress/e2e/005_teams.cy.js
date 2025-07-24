c1a230842e510c037aa3d6e345d01b22c311f1e4
Blake Rain <blake.rain@blakerain.com>
2025-07-24 20:27:27 +0100

    fix: Cypress tests (adding a little bit too)

--- cypress/cypress/e2e/005_teams.cy.js
+++ cypress/cypress/e2e/005_teams.cy.js
@@ -82,11 +82,11 @@ describe("Teams", () => {
     cy.get(".modal > .content h1").should("contain", "Edit user (admin)");
 
     cy.get("@teamId").then((teamId) => {
-      cy.get(`.modal > .content #team-${teamId}`)
+      cy.get(`.modal > .content #target-${teamId}`)
         .should("not.be.checked")
         .check();
 
-      cy.get(`.modal > .content #team-${teamId}`)
+      cy.get(`.modal > .content #target-${teamId}`)
         .next()
         .next()
         .should("not.be.checked")
@@ -265,5 +265,126 @@ describe("Teams", () => {
         new Date().toISOString().split("T")[0],
       );
     });
+  });
+
+  it("Team configuration", () => {
+    // Add a new team
+    cy.visit("/admin/teams");
+
+    cy.get("button").contains("Add Team").click();
+
+    cy.get(".modal > .content").should("be.visible");
+    cy.get(".modal > .content h1").should("contain", "Add new team");
+
+    cy.get(".modal > .content input[name='name']").type("First Team");
+    cy.get(".modal > .content input[name='slug']").should(
+      "have.value",
+      "first-team",
+    );
+
+    cy.get(".modal > .content button[type='submit']").click();
+
+    // Get the team ID from the `<parcel-clipboard>` element's `value` attribute.
+    cy.get(
+      "#teams-list-container table > tbody > tr:first-child > td:first-child parcel-clipboard",
+    )
+      .invoke("attr", "value")
+      .should("not.be.empty")
+      .as("teamId");
+
+    // Add user to the team
+    cy.visit("/admin/users");
+
+    cy.contains("#user-list-container table > tbody > tr", "admin").within(
+      () => {
+        cy.get("td:nth-child(12) .dropdown-button").click();
+        cy.get("a[title='Edit user']").click();
+      },
+    );
+
+    cy.get(".modal > .content").should("be.visible");
+    cy.get(".modal > .content h1").should("contain", "Edit user (admin)");
+
+    cy.get("@teamId").then((teamId) => {
+      cy.get(`.modal > .content #target-${teamId}`)
+        .should("not.be.checked")
+        .check();
+
+      cy.get(`.modal > .content #target-${teamId}`)
+        .next()
+        .next()
+        .should("not.be.checked")
+        .check()
+        .next()
+        .should("not.be.checked")
+        .check();
+    });
+
+    cy.get(".modal > .content button[type='submit']").click();
+
+    // Visit the team page and make sure the configuration option is not available yet.
+    cy.visit("/teams/first-team");
+    cy.get("#team-settings-button").should("not.exist");
+
+    // Go back to the user page and edit the team permissions for the user.
+    cy.visit("/admin/users");
+
+    cy.contains("#user-list-container table > tbody > tr", "admin").within(
+      () => {
+        cy.get("td:nth-child(12) .dropdown-button").click();
+        cy.get("a[title='Edit user']").click();
+      },
+    );
+
+    cy.get(".modal > .content").should("be.visible");
+    cy.get(".modal > .content h1").should("contain", "Edit user (admin)");
+
+    cy.get("@teamId").then((teamId) => {
+      cy.get(`.modal > .content #target-${teamId}`)
+        .next()
+        .next()
+        .next()
+        .next()
+        .should("not.be.checked")
+        .check();
+    });
+
+    cy.get(".modal > .content button[type='submit']").click();
+
+    // Visit the team page and make sure the configuration option is avaialable now.
+    cy.visit("/teams/first-team");
+    cy.get("#team-settings-button").should("be.visible").click();
+
+    // Check the modal is open and that the team name is correct, then change it.
+    cy.get(".modal > .content").should("be.visible");
+    cy.get(".modal > .content input[name='name']")
+      .should("have.value", "First Team")
+      .clear()
+      .type("New Team Name");
+
+    cy.get(".modal > .content button[type='submit']").click();
+
+    cy.url().should("eq", Cypress.config().baseUrl + "/teams/first-team");
+    cy.get("#team-tab-list > a:nth-child(1)").should("contain", "Your Uploads");
+    cy.get("#team-tab-list > a:nth-child(2)").should(
+      "contain",
+      "New Team Name",
+    );
+
+    // Open the team settings modal again and change the slug.
+    cy.get("#team-settings-button").should("be.visible").click();
+    cy.get(".modal > .content input[name='slug']").should(
+      "have.value",
+      "first-team",
+    );
+    cy.get(".modal > .content input[name='slug']")
+      .should("have.value", "first-team")
+      .clear()
+      .type("my-first-team");
+
+    // Let the modal settle.
+    cy.wait(1000);
+
+    cy.get(".modal > .content button[type='submit']").click();
   });
 });
