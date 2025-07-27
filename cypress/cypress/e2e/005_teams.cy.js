6f6ab1e799a323c9676a34e407361231103d479b
Blake Rain <blake.rain@blakerain.com>
2025-07-27 13:32:27 +0100

    chore: remove some teams settings code from teams E2E test

--- cypress/cypress/e2e/005_teams.cy.js
+++ cypress/cypress/e2e/005_teams.cy.js
@@ -18,51 +18,51 @@ describe("Teams", () => {
     cy.get(".modal > .content input[name='name']").type("First Team");
     cy.get(".modal > .content input[name='slug']").should(
       "have.value",
-      "first-team",
+      "first-team"
     );
 
     cy.get(".modal > .content button[type='submit']").click();
 
     // Get the team ID from the `<parcel-clipboard>` element's `value` attribute.
     cy.get(
-      "#teams-list-container table > tbody > tr:first-child > td:first-child parcel-clipboard",
+      "#teams-list-container table > tbody > tr:first-child > td:first-child parcel-clipboard"
     )
       .invoke("attr", "value")
       .should("not.be.empty")
       .as("teamId");
 
     cy.get(
-      "#teams-list-container table > tbody > tr:first-child > td:nth-child(2)",
+      "#teams-list-container table > tbody > tr:first-child > td:nth-child(2)"
     ).should("contain", "First Team");
 
     // Enabled
     cy.get(
-      "#teams-list-container table > tbody > tr:first-child > td:nth-child(3)",
+      "#teams-list-container table > tbody > tr:first-child > td:nth-child(3)"
     ).should("contain", "Yes");
 
     // Members
     cy.get(
-      "#teams-list-container table > tbody > tr:first-child > td:nth-child(4)",
+      "#teams-list-container table > tbody > tr:first-child > td:nth-child(4)"
     ).should("contain", "0");
 
     // Uploads
     cy.get(
-      "#teams-list-container table > tbody > tr:first-child > td:nth-child(5)",
+      "#teams-list-container table > tbody > tr:first-child > td:nth-child(5)"
     ).should("contain", "0");
 
     // Total
     cy.get(
-      "#teams-list-container table > tbody > tr:first-child > td:nth-child(6)",
+      "#teams-list-container table > tbody > tr:first-child > td:nth-child(6)"
     ).should("contain", "0");
 
     // Limit
     cy.get(
-      "#teams-list-container table > tbody > tr:first-child > td:nth-child(7)",
+      "#teams-list-container table > tbody > tr:first-child > td:nth-child(7)"
     ).should("contain", "1 GB");
 
     // Created at
     cy.get(
-      "#teams-list-container table > tbody > tr:first-child > td:nth-child(8)",
+      "#teams-list-container table > tbody > tr:first-child > td:nth-child(8)"
     ).should("contain", new Date().toISOString().split("T")[0]);
 
     // Add user to the team
@@ -75,7 +75,7 @@ describe("Teams", () => {
 
         cy.get("td:nth-child(12) .dropdown-button").click();
         cy.get("a[title='Edit user']").click();
-      },
+      }
     );
 
     cy.get(".modal > .content").should("be.visible");
@@ -102,7 +102,7 @@ describe("Teams", () => {
       () => {
         // Admin is now in one team.
         cy.get("td:nth-child(8)").should("contain", "1");
-      },
+      }
     );
 
     // Go back to the teams list
@@ -110,11 +110,11 @@ describe("Teams", () => {
 
     // Check that the team has one member, but still no uploads.
     cy.get(
-      "#teams-list-container table > tbody > tr:first-child > td:nth-child(4)",
+      "#teams-list-container table > tbody > tr:first-child > td:nth-child(4)"
     ).should("contain", "1");
 
     cy.get(
-      "#teams-list-container table > tbody > tr:first-child > td:nth-child(5)",
+      "#teams-list-container table > tbody > tr:first-child > td:nth-child(5)"
     ).should("contain", "0");
 
     // ------------------------------------------------------------------------
@@ -131,7 +131,7 @@ describe("Teams", () => {
     // There should be no uploads for the user yet.
     cy.get("#uploads-table").should(
       "contain",
-      "You have not uploaded any files",
+      "You have not uploaded any files"
     );
 
     // Upload a file to the user's uploads.
@@ -153,7 +153,7 @@ describe("Teams", () => {
       cy.get("div:nth-child(3)").should("contain", "69 B");
       cy.get("div:nth-child(8)").should(
         "contain",
-        new Date().toISOString().split("T")[0],
+        new Date().toISOString().split("T")[0]
       );
     });
 
@@ -164,14 +164,14 @@ describe("Teams", () => {
     cy.url().should("eq", Cypress.config().baseUrl + "/teams/first-team");
     cy.get("#team-tab-list > a:nth-child(1)").should(
       "not.have.class",
-      "active",
+      "active"
     );
     cy.get("#team-tab-list > a:nth-child(2)").should("have.class", "active");
 
     // There should be no uploads for the team yet.
     cy.get("#uploads-table").should(
       "contain",
-      "You have not uploaded any files",
+      "You have not uploaded any files"
     );
 
     // Upload a file to the team's uploads.
@@ -193,7 +193,7 @@ describe("Teams", () => {
       cy.get("div:nth-child(3)").should("contain", "30 B");
       cy.get("div:nth-child(8)").should(
         "contain",
-        new Date().toISOString().split("T")[0],
+        new Date().toISOString().split("T")[0]
       );
     });
 
@@ -205,7 +205,7 @@ describe("Teams", () => {
     cy.get("#team-tab-list > a:nth-child(1)").should("have.class", "active");
     cy.get("#team-tab-list > a:nth-child(2)").should(
       "not.have.class",
-      "active",
+      "active"
     );
 
     // Make sure the upload is listed in the user's uploads.
@@ -216,7 +216,7 @@ describe("Teams", () => {
       cy.get("div:nth-child(3)").should("contain", "69 B");
       cy.get("div:nth-child(8)").should(
         "contain",
-        new Date().toISOString().split("T")[0],
+        new Date().toISOString().split("T")[0]
       );
     });
 
@@ -227,7 +227,7 @@ describe("Teams", () => {
     cy.url().should("eq", Cypress.config().baseUrl + "/teams/first-team");
     cy.get("#team-tab-list > a:nth-child(1)").should(
       "not.have.class",
-      "active",
+      "active"
     );
     cy.get("#team-tab-list > a:nth-child(2)").should("have.class", "active");
 
@@ -239,7 +239,7 @@ describe("Teams", () => {
       cy.get("div:nth-child(3)").should("contain", "30 B");
       cy.get("div:nth-child(8)").should(
         "contain",
-        new Date().toISOString().split("T")[0],
+        new Date().toISOString().split("T")[0]
       );
     });
 
@@ -249,7 +249,7 @@ describe("Teams", () => {
     cy.get("#team-tab-list > a:nth-child(1)").should("have.class", "active");
     cy.get("#team-tab-list > a:nth-child(2)").should(
       "not.have.class",
-      "active",
+      "active"
     );
 
     // ------------------------------------------------------------------------
@@ -262,7 +262,7 @@ describe("Teams", () => {
       cy.get("div:nth-child(3)").should("contain", "69 B");
       cy.get("div:nth-child(8)").should(
         "contain",
-        new Date().toISOString().split("T")[0],
+        new Date().toISOString().split("T")[0]
       );
     });
   });
@@ -279,14 +279,14 @@ describe("Teams", () => {
     cy.get(".modal > .content input[name='name']").type("First Team");
     cy.get(".modal > .content input[name='slug']").should(
       "have.value",
-      "first-team",
+      "first-team"
     );
 
     cy.get(".modal > .content button[type='submit']").click();
 
     // Get the team ID from the `<parcel-clipboard>` element's `value` attribute.
     cy.get(
-      "#teams-list-container table > tbody > tr:first-child > td:first-child parcel-clipboard",
+      "#teams-list-container table > tbody > tr:first-child > td:first-child parcel-clipboard"
     )
       .invoke("attr", "value")
       .should("not.be.empty")
@@ -299,7 +299,7 @@ describe("Teams", () => {
       () => {
         cy.get("td:nth-child(12) .dropdown-button").click();
         cy.get("a[title='Edit user']").click();
-      },
+      }
     );
 
     cy.get(".modal > .content").should("be.visible");
@@ -333,7 +333,7 @@ describe("Teams", () => {
       () => {
         cy.get("td:nth-child(12) .dropdown-button").click();
         cy.get("a[title='Edit user']").click();
-      },
+      }
     );
 
     cy.get(".modal > .content").should("be.visible");
@@ -353,38 +353,6 @@ describe("Teams", () => {
 
     // Visit the team page and make sure the configuration option is avaialable now.
     cy.visit("/teams/first-team");
-    cy.get("#team-settings-button").should("be.visible").click();
-
-    // Check the modal is open and that the team name is correct, then change it.
-    cy.get(".modal > .content").should("be.visible");
-    cy.get(".modal > .content input[name='name']")
-      .should("have.value", "First Team")
-      .clear()
-      .type("New Team Name");
-
-    cy.get(".modal > .content button[type='submit']").click();
-
-    cy.url().should("eq", Cypress.config().baseUrl + "/teams/first-team");
-    cy.get("#team-tab-list > a:nth-child(1)").should("contain", "Your Uploads");
-    cy.get("#team-tab-list > a:nth-child(2)").should(
-      "contain",
-      "New Team Name",
-    );
-
-    // Open the team settings modal again and change the slug.
-    cy.get("#team-settings-button").should("be.visible").click();
-    cy.get(".modal > .content input[name='slug']").should(
-      "have.value",
-      "first-team",
-    );
-    cy.get(".modal > .content input[name='slug']")
-      .should("have.value", "first-team")
-      .clear()
-      .type("my-first-team");
-
-    // Let the modal settle.
-    cy.wait(1000);
-
-    cy.get(".modal > .content button[type='submit']").click();
+    cy.get("#team-settings-button").should("be.visible");
   });
 });
